e3294c50ab47f3ded97b41ef692619530cf1f305
Dijar06 <dize06001@utb.vaxjo.se>
2024-02-28 10:56:03 +0100

    Add project files.

---
+++ Uppgift 6.12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bestäm triangelns höjd:");
+            int höjd = int.Parse(Console.ReadLine());
+            Triangel(höjd);
+        }
+
+        static void Triangel(int höjd)
+        {
+            for (int i = 0; i < höjd; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
